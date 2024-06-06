6ab494934f7a0aac10cb2aa0a4d7e30e96ea304a
moarshy <marshath@gmail.com>
2024-06-06 12:29:54 +0800

    use openai

--- babyagi_task_finalizer/run.py
+++ babyagi_task_finalizer/run.py
@@ -1,10 +1,39 @@
-from babyagi_task_finalizer.schemas import InputSchema
-from babyagi_task_finalizer.utils import get_logger
-from litellm import completion
+import os
 import yaml
+import instructor
+from litellm import Router
+from babyagi_task_finalizer.schemas import InputSchema, TaskFinalizer
+from babyagi_task_finalizer.utils import get_logger
 
 
 logger = get_logger(__name__)
+
+client = instructor.patch(
+    Router(
+        model_list=
+        [
+            {
+                "model_name": "gpt-3.5-turbo",
+                "litellm_params": {
+                    "model": "openai/gpt-3.5-turbo",
+                    "api_key": os.getenv("OPENAI_API_KEY"),
+                },
+            }
+        ],
+        # default_litellm_params={"acompletion": True},
+    )
+)
+
+def llm_call(messages, response_model=None):
+    if response_model:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            response_model=response_model,
+            messages=messages,
+            temperature=0.0,
+            max_tokens=1000,
+        )
+    return response
 
 def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None, flow_run = None, cfg: dict = None):
     logger.info(f"Running with inputs {inputs.objective}")
@@ -17,17 +46,11 @@ def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None, flow
         {"role": "user", "content": user_prompt}
     ]
 
-    result = completion(
-        model=cfg["models"]["ollama"]["model"],
-        messages=messages,
-        temperature=cfg["models"]["ollama"]["temperature"],
-        max_tokens=cfg["models"]["ollama"]["max_tokens"],
-        api_base=cfg["models"]["ollama"]["api_base"],
-    ).choices[0].message.content
+    response = llm_call(messages, response_model=TaskFinalizer)
 
-    logger.info(f"Result: {result}")
+    logger.info(f"Result: {response}")
 
-    return result
+    return response.model_dump_json()
 
 
 if __name__ == "__main__":
@@ -35,8 +58,15 @@ if __name__ == "__main__":
         cfg = yaml.safe_load(f)
 
     inputs = InputSchema(
-        task="Weather pattern between year 1900 and 2000?", 
+        task="Weather pattern between year 1900 and 2000 was cool andry", 
         objective="Write a blog post about the weather in London."
     )
 
-    run(inputs, cfg=cfg)
+    r = run(inputs, cfg=cfg)
+    logger.info(f"Result: {type(r)}")
+
+
+    import json
+    t = TaskFinalizer(**json.loads(r))
+    logger.info(f"Final report: {type(t)}")
+
--- babyagi_task_finalizer/schemas.py
+++ babyagi_task_finalizer/schemas.py
@@ -1,5 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List
 
 class InputSchema(BaseModel):
     task: str
     objective: str
+
+class Task(BaseModel):
+    """Class for defining a task to be performed."""
+    name: str = Field(..., description="The name of the task to be performed.")
+    description: str = Field(..., description="The description of the task to be performed.")
+    done: bool = Field(False, description="The status of the task. True if the task is done, False otherwise.")
+    result: str = Field("", description="The result of the task.")
+
+class TaskFinalizer(BaseModel):
+    """Class for finalizing the tasks."""
+    final_report: str = Field("", description="The final report of the tasks.")
+    new_tasks: List[Task] = Field([], description="A list of new tasks to be performed.")
+    objective_met: bool = Field(False, description="The status of the objective. True if the objective have been met, False otherwise.")
