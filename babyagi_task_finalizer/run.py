08b73335a6c6f0350206065e0e63c1c49e8a2dda
Richard Blythman <richardblythman@gmail.com>
2024-09-30 18:46:37 +0100

    use args kwargs for run

--- babyagi_task_finalizer/run.py
+++ babyagi_task_finalizer/run.py
@@ -35,9 +35,10 @@ def llm_call(messages, response_model=None):
         )
     return response
 
-def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None, flow_run = None, cfg: dict = None):
+def run(inputs: InputSchema, *args, **kwargs):
     logger.info(f"Running with inputs {inputs.objective}")
     logger.info(f"Running with inputs {inputs.task}")
+    cfg = kwargs["cfg"]
 
     user_prompt = cfg["inputs"]["user_message_template"].replace("{{task}}", inputs.task).replace("{{objective}}", inputs.objective)
 
